b5cf96bf8e30f7d0c889fb578ad70abf76493011
Matías Rojas Rojas <69127196+matias-rojas-dev@users.noreply.github.com>
2022-05-02 03:46:41 -0400

    Add files via upload

--- HomeClient.aspx.cs
+++ HomeClient.aspx.cs
@@ -6,7 +6,7 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Text.RegularExpressions;
-
+using System.IO;
 
 namespace WSClientSoap
 {
@@ -15,18 +15,15 @@ namespace WSClientSoap
 
         WSServerSOAP.MetodosRCDClient ws = new WSServerSOAP.MetodosRCDClient();
 
-        protected void Page_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        protected void checkCode_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
         protected void name_result_Click(object sender, EventArgs e)
         {
+
+            string path = @"\logs-g3-rcd";
+            string setPath = Path.GetFullPath(path);
+            Log myLog = new Log(@setPath);
+
+            myLog.Add("Llamado al método 'ValidacionNombre'");
+
             String dato = name_inputCompleteName.Text.ToString();
 
             StringBuilder sb = new StringBuilder();
@@ -41,6 +38,7 @@ namespace WSClientSoap
 
                 if (sizeName > 4)
                 {
+                    myLog.Add("Ha ingresado correctamente los campos necesarios para realizar la operación");
                     int[] mainValues = { name.Length - 3 };
                     foreach (var item in name)
                     { // 0
@@ -61,40 +59,56 @@ namespace WSClientSoap
                         sb.Append("</ul>");
                     } // end first for
 
+                    myLog.Add("No ha habido problemas. Se ha completado exitosamente la operación ");
                     litMarkup.Text = sb.ToString();
                 }
                 else
                 {
                         name_response.Text = "Por favor, ingrese como mínimo dos apellidos";
+                        myLog.Add("Error: el usuario no ha ingresado la cantidad mínima de nombres");
+
                 }
             }
             catch
             {
                 name_response.Text = "Ha ingresado solo un nombre. Por favor, ingrese dos apellidos";
+                myLog.Add("Error: el usuario no ha ingresado la cantidad mínima de nombres: solo ha ingresado un nombre o apellido");
             }
         }
 
         protected void rut_result_Click(object sender, EventArgs e)
         {
+            string path = @"\logs-g3-rcd";
+            string setPath = Path.GetFullPath(path);
+            Log myLog = new Log(@setPath);
+            myLog.Add("Llamado al método 'ValidacionRut'");
+
             String rut = rut_withoutCheckCode.Text.ToString();
             String dv = rut_checkCode.Text.ToString();
             String[] posibleValuesDv = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "K", "k" };
             var check = Array.Exists(posibleValuesDv, element => element == dv);
+
+            myLog.Add("Se han recibido tanto el rut sin dígito verificador como el mismo dígito verificador");
 
             if (check == true)
             {
                 if ((ws.ValidacionRut(rut, dv).ToString()).Equals("True"))
                 {
                     rut_response.Text = "El digito validador ingresado es correcto";
+                    myLog.Add("No ha habido problemas. Se ha completado exitosamente la operación");
                 }
                 else
                 {
                     rut_response.Text = "Oops! El digito verificador no corresponde";
+                    myLog.Add("Error: el dígito verificador que ha ingresado no corresponde al RUT");
+
                 }
             }
             else
             {
                 rut_response.Text = "Error en el dígito verificador";
+                myLog.Add("Error: el dígito que ha ingresado no está permitido");
+
             }
         }
     }
---
+++ Log.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.IO;
+
+namespace WSClientSoap
+{
+    public class Log
+    {
+        private string Path = "";
+
+        public Log(string Path)
+        {
+            this.Path = Path;
+        }
+
+        public void Add(string sLog)
+        {
+            CreateDirectory();
+            string name = GetNameFile();
+            string text = "";
+
+            text+=DateTime.Now+" - "+sLog + Environment.NewLine;
+
+            StreamWriter sw = new StreamWriter(Path+"/"+name,true);
+            sw.Write(text);
+            sw.Close();
+        }
+
+        #region HELPER
+        private string GetNameFile()
+        {
+            string name = "";
+            name = "log_" + DateTime.Now.Day+"_"+DateTime.Now.Month+"_"+DateTime.Now.Year+".txt";  
+            return name;
+        }
+        private bool CreateDirectory()
+        {
+            try
+            {
+                if (!Directory.Exists(Path))
+                    Directory.CreateDirectory(Path);
+
+                return true;
+            } 
+            catch(DirectoryNotFoundException ex)
+            {
+                throw new Exception(ex.Message);
+                return false;
+            }
+        }
+        #endregion
+    }
+}
